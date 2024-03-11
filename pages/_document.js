101ebe988aa8c1bbce6210b48df2206c9b8081ad
Abhinav-FP <a.mathur@futureprofilez.com>
2024-03-11 16:29:31 +0530

    vercel

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"
         />
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
