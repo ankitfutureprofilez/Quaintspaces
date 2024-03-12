b90e756e2e9b361d77cfe9dc4a173487285f13ad
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-12 11:13:34 +0530

    vercel json 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/globals.css";
 import '../styles/customs.css';
 import "../styles/fonts.css"
 import '../styles/RangeSlider.css';
-
+import { useRouter } from 'next/router';
 export const Context = React.createContext();
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   const [wishlist, setWishlist] = useState(false);
   const [wishlistData, setWishlistData] = useState(null);
 
   const values = { wishlist, setWishlist, wishlistData, setWishlistData };
+
+  useEffect(() => {
+    if (router.pathname === '/') {
+      router.push('/'); 
+    }
+  }, [router.pathname]);
 
   return (
     <Context.Provider value={values}>
