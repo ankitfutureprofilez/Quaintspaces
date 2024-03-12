9ecbb19da19d244bbec7e646b0cfa6d74d610f27
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-12 10:51:47 +0530

    vercel json 2

--- pages/custom/Navbar.js
+++ pages/custom/Navbar.js
@@ -37,13 +37,13 @@ export default function Navbar() {
             </Link>
           </div>
 
-          <Popup
+          {/* <Popup
             isOpen={isOpen}
             togglePopup={togglePopup}
             space={2}
             text={"Hello  World ankit"}
-          />
-          {isOpen && (
+          /> */}
+          {/* {isOpen && (
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -51,7 +51,7 @@ export default function Navbar() {
             >
               Close modal
             </button>
-          )}
+          )} */}
         </div>
       </div>
     </nav>
