389f7cff3ea7d56d8ddf189d60020d3ad7cdd719
Abhinav-FP <a.mathur@futureprofilez.com>
2024-03-13 17:53:24 +0530

    gray color added

--- pages/listings/ThingsToKnow.js
+++ pages/listings/ThingsToKnow.js
@@ -7,18 +7,18 @@ export default function ThingsToKnow() {
       <div className="flex justify-between">
         <div className="flex flex-col my-8 mr-4">
           <h2 className="font-semibold mb-2">House Rules</h2>
-          <p className="mb-2">Check-in after 02:00 pm</p>
-          <p className="mb-2">Checkout before 10:00 am</p>
-          <p className="mb-2">6 Guests Maximum</p>
+          <p className="mb-2 text-gray-500">Check-in after 02:00 pm</p>
+          <p className="mb-2 text-gray-500">Checkout before 10:00 am</p>
+          <p className="mb-2 text-gray-500">6 Guests Maximum</p>
         </div>
         <div className="flex flex-col my-8 mr-4">
           <h2 className="font-semibold mb-2">Safety & Property </h2>
-          <p className="mb-2">Carbon monoxide alarm</p>
-          <p className="mb-2">Smoke Alarm</p>
+          <p className="mb-2 text-gray-500">Carbon monoxide alarm</p>
+          <p className="mb-2 text-gray-500">Smoke Alarm</p>
         </div>
         <div className="flex flex-col my-8 mr-4">
           <h2 className="font-semibold mb-2">House Rules</h2>
-          <p className="mb-2">This reservation is non-refundable.</p>
+          <p className="mb-2 text-gray-500">This reservation is non-refundable.</p>
         </div>
       </div>
     </div>
