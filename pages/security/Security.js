d80531adefe265b7c0bab875afb484fa38a518d6
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-13 12:35:33 +0530

    add

--- pages/security/Security.js
+++ pages/security/Security.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react'
 import Heading from '../elements/Heading'
 import Button from '../elements/Button'
-
-
-
 export default function Security() {
 
     const [formData, setFormData] = useState({
