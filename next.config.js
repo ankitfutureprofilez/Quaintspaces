8ed91cf00c56da71a042c503c46229e92ba953ea
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-18 17:39:25 +0530

    merge

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["a0.muscache.com"],
   },
+  webpack5: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
-
 module.exports = nextConfig;
