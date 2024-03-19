84ac1ca8cd5421557e109713f4ea5a2143883af3
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-19 10:45:42 +0530

    xdd

--- pages/admin/app/app/login/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Login from "./Login"; 
-
-export default function Index() {
-  return (
-    <div>
-    <Login/>
-    </div>
-  );}
--- pages/admin/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Page from "./app/page"
-
-export default function index() {
-  return (
-      <Page />
-  );
-}
