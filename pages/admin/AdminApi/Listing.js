33d520372e970cc3139e52f83f31bbddf069913b
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-19 10:23:25 +0530

    admin setup

--- pages/admin/AdminApi/Listing.js
+++ pages/admin/AdminApi/Listing.js
@@ -4,16 +4,12 @@ class Listing extends Component {
     async adminlogin(data) {
         return Api.post("/admin/login",data);
     }
-    
     async addproperty(data) {
         return Api.post("/admin/add-property",data);
     }
-    
-
     async city_list(id){
         return Api.get(`/admin/city-list/${id}`)
     }
-
     async area_list(id){
         return Api.get(`/admin/area-list/${id}`)
     }
