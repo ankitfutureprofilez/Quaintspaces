45663fb1f4026b97bf8d21a015ad30b6987a9d00
Abhinav-FP <a.mathur@futureprofilez.com>
2024-03-19 18:21:25 +0530

    Image Issue Resolved

--- pages/book/[listingID].js
+++ pages/book/[listingID].js
@@ -12,19 +12,19 @@ import Footer from "../../components/Footer";
 import { getParams } from "../../utils/handlers";
 import postsData from "../../bot/data.json";
 import Heading from "../elements/Heading";
-import Image from 'next/image'
+import Image from "next/image";
 import Button from "../elements/Button";
 // import Listings from "../LaravelApi/Listings";
 
 const Book = () => {
   const router = useRouter();
   const { listingID } = router.query;
-  console.log("listingID",listingID)
+  console.log("listingID", listingID);
   const [listing, setListing] = useState([]);
   const [infos, setInfos] = useState({});
   const [dateModel, setDateModel] = useState(false);
   const [guestsModel, setGuestsModel] = useState(false);
-  console.log("infos",infos)
+  console.log("infos", infos);
   const [guests, setGuests] = useState({
     adults: {
       value: +infos.adults || 0,
@@ -59,7 +59,7 @@ const Book = () => {
   //     }).catch((err)=>{
   //       console.error(err);
   //     });
-  
+
   //     setGuests({
   //       adults: {
   //         value: +url.numberOfAdults || 0,
@@ -86,23 +86,23 @@ const Book = () => {
   // }, [router.asPath]);
 
   const [formData, setFormData] = useState({
-    selectOption: '',
-    file: null
+    selectOption: "",
+    file: null,
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      file: file
+      file: file,
     }));
   };
 
@@ -260,7 +260,10 @@ const Book = () => {
 
     <div>
       <main className="max-w-[1150px] min-h-screen py-[3.6rem] mx-auto pt-12">
-        <Heading text={"Confirm and pay"} handleClick={() => router.push("-1")} />
+        <Heading
+          text={"Confirm and pay"}
+          handleClick={() => router.push("-1")}
+        />
         <div className="flex mt-14 px-3 gap-10">
           <div className="w-8/12">
             <h1 className="text-xl mb-4 font-medium heading-data">Your trip</h1>
@@ -275,7 +278,6 @@ const Book = () => {
                 } - ${
                   infos?.checkout && format(new Date(infos.checkout), "MMM dd")
                 }`}</h5>
-
               </div>
               <button
                 onClick={() => setDateModel(true)}
@@ -299,7 +301,7 @@ const Book = () => {
                   }`}
                 </h5> */}
                 <h5 className="text-md item-paragrapg">
-                {`${+infos.numberOfAdults + +infos.numberOfChildren} guests ${
+                  {`${+infos.numberOfAdults + +infos.numberOfChildren} guests ${
                     +infos.numberOfInfants
                       ? ", " + infos.numberOfInfants + " infants"
                       : ""
@@ -317,114 +319,108 @@ const Book = () => {
                 EDIT
               </button>
             </div>
-            <h1 className="text-xl mb-4 font-medium mt-5 heading-data">Upload ID</h1>
+            <h1 className="text-xl mb-4 font-medium mt-5 heading-data">
+              Upload ID
+            </h1>
             <div className=" border-b border-borderColor">
-                <form onSubmit={handleSubmit}>
-                  <div className="mb-4">
-                    <select
-                      id="selectOption"
-                      name="selectOption"
-                      value={formData.selectOption}
-                      onChange={handleChange}
-                      className="mt-1 p-4 border rounded-full w-full"
-                      required
-                    >
-                      <option value="">Choose...</option>
-                      <option value="option1">Option 1</option>
-                      <option value="option2">Option 2</option>
-                      <option value="option3">Option 3</option>
-                    </select>
-                  </div>
-
-                  <div className="mb-4">
-                    <input
-                      type="file"
-                      id="fileUpload"
-                      name="fileUpload"
-                      accept=".pdf,.doc,.docx"
-                      onChange={handleFileChange}
-                      className="mt-1 p-4 border rounded-full w-full"
-                      required
-                    />
-                  </div>
-                </form>
-            </div>
-            <h1 className="text-xl mb-4 font-medium mt-5 heading-data">Required for your trip</h1>
-            <div className='flex items-center justify-between w-full py-2 pb-4 border-b border-borderColor'>
-
-              <div className="ml-3 mt-4">
-                <h1 className="text-lg  item-heading mb-2" >
-                  Message the host
-                </h1>
-                <div className="flex flex-wrap justify-between mb-5">
-                  <p className="item-pargraph"> Share why you're travelling, who's coming with you and what you love about the space.
-                  </p>
-                  <p  className="edit-color underline">
-                    ADD
-                  </p>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <select
+                    id="selectOption"
+                    name="selectOption"
+                    value={formData.selectOption}
+                    onChange={handleChange}
+                    className="mt-1 p-4 border rounded-full w-full"
+                    required
+                  >
+                    <option value="">Choose...</option>
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                  </select>
                 </div>
 
-                <h1 className="text-lg item-heading mb-2">
-                  Phone number
-                </h1>
+                <div className="mb-4">
+                  <input
+                    type="file"
+                    id="fileUpload"
+                    name="fileUpload"
+                    accept=".pdf,.doc,.docx"
+                    onChange={handleFileChange}
+                    className="mt-1 p-4 border rounded-full w-full"
+                    required
+                  />
+                </div>
+              </form>
+            </div>
+            <h1 className="text-xl mb-4 font-medium mt-5 heading-data">
+              Required for your trip
+            </h1>
+            <div className="flex items-center justify-between w-full py-2 pb-4 border-b border-borderColor">
+              <div className="ml-3 mt-4">
+                <h1 className="text-lg  item-heading mb-2">Message the host</h1>
+                <div className="flex flex-wrap justify-between mb-5">
+                  <p className="item-pargraph">
+                    {" "}
+                    Share why you're travelling, who's coming with you and what
+                    you love about the space.
+                  </p>
+                  <p className="edit-color underline">ADD</p>
+                </div>
+
+                <h1 className="text-lg item-heading mb-2">Phone number</h1>
                 <div className="flex flex-wrap justify-between">
                   <p className="item-pargraph">
                     Add and confirm your phone number to get trip updates.
                   </p>
-                  <p  className="edit-color underline">
-                    ADD
-                  </p>
+                  <p className="edit-color underline">ADD</p>
                 </div>
               </div>
-
             </div>
-          <div className='flex items-center justify-between w-full py-2 pb-4 border-b border-borderColor'>
-            <div className="ml-3 mt-4">
-              <h1 className="text-lg heading-data mb-4">
-              Cancellation policy
-              </h1>
-              <div className="flex flex-wrap ">
-                <p className="item-pargraph">
-                  This reservation is non-refundable.
-                </p>
-                <p  className="underline edit-color">
-                Learn More
-                </p>
+            <div className="flex items-center justify-between w-full py-2 pb-4 border-b border-borderColor">
+              <div className="ml-3 mt-4">
+                <h1 className="text-lg heading-data mb-4">
+                  Cancellation policy
+                </h1>
+                <div className="flex flex-wrap ">
+                  <p className="item-pargraph">
+                    This reservation is non-refundable.
+                  </p>
+                  <p className="underline edit-color">Learn More</p>
+                </div>
               </div>
             </div>
-          </div>
-         <div className="mt-5">
-          <Button text={"Confirm & Pay"} 
-                    design={"font-inter font-lg leading-tight text-center text-white w-96 bg-orange-300  border-2 p-4 rounded-full"} />
-          </div>
+            <div className="mt-5">
+              <Button
+                text={"Confirm & Pay"}
+                design={
+                  "font-inter font-lg leading-tight text-center text-white w-96 bg-orange-300  border-2 p-4 rounded-full"
+                }
+              />
+            </div>
           </div>
           <div className="w-5/12  rounded-xl shadow py-8 px-5 h-fit golden-border">
             <div className="flex gap-3 pb-4 border-b border-borderColor image-data">
-            <Image 
-    src="http://quaintstays.laraveldevelopmentcompany.com//public//storage//property//images//1710834595_houseimg%202.jpg"
-    alt="Apartment"
-    width={200}
-    height={200}
-/>
+              {/* <Image src="" alt="Apartment" width={200} height={200} /> */}
 
-
-              {/* <img
+              <img
                 src={
                   listing?.images?.length > 0
                     ? listing.images[0].url
                     : "https://a0.muscache.com/im/pictures/ed3c3933-428a-435b-9161-196722bcf63d.jpg?aki_policy=large"
                 }
                 className="w-32 h-28 rounded-lg object-cover"
-              /> */}
+              />
               <div>
                 <h4 className="text-xl mb-1">{listing?.title}</h4>
-               <h3 className=" text-lg">Entire Apartment </h3>
+                <h3 className=" text-lg">Entire Apartment </h3>
                 <span className="flex text-sm items-center gap-1">
                   <span>
                     <Star />
                   </span>
                   <span>
-                    {listing?.rating || "4.5"} ({listing?.reviews_length || 141} reviews)
+                    {listing?.rating || "4.5"} ({listing?.reviews_length || 141}{" "}
+                    reviews)
                   </span>
                 </span>
               </div>
@@ -446,14 +442,12 @@ const Book = () => {
                     Nights
                   </span>
                   <span className="block text-blackColor font-medium">
-                  {
-                      infos.checkout &&
+                    {infos.checkout &&
                       infos.checkin &&
                       differenceInDays(
                         new Date(infos.checkout),
                         new Date(infos.checkin)
-                      )
-                    }
+                      )}
                   </span>
                 </div>
               </div>
@@ -472,29 +466,22 @@ const Book = () => {
                     Charges Per Day
                   </span>
                   <span className="block text-blackColor font-medium confirm-price">
-                  ₹{listing?.price}
+                    ₹{listing?.price}
                   </span>
                 </div>
-
-                
-
               </div>
-
-              
-
-             
             </div>
             <div className="pt-4 flex items-center justify-between confirm-total">
               <span className="">Total(INR)</span>
               <span className="text-md font-medium">
-              ₹
-                    {infos.checkout &&
-                      infos.checkin &&
-                      +listing?.price*
-                        differenceInDays(
-                          new Date(infos.checkout),
-                          new Date(infos.checkin)
-                        )}
+                ₹
+                {infos.checkout &&
+                  infos.checkin &&
+                  +listing?.price *
+                    differenceInDays(
+                      new Date(infos.checkout),
+                      new Date(infos.checkin)
+                    )}
               </span>
             </div>
           </div>
