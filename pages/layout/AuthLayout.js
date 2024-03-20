c6c4b3919ab926ae4404b0796ad9501d066e3aff
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-20 14:27:49 +0530

    Api intewrgration

--- pages/layout/AuthLayout.js
+++ pages/layout/AuthLayout.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import HeroBanner from '../home/HeroBanner';
-import Footer from '../home/Footer';
+import React, { useState, useEffect, createContext } from 'react';
+import { useLocation, navigate } from 'react-router-dom';
+import { toast } from 'react-toastify'; 
 import Header from '../home/Header';
-
+import Footer from '../home/Footer';
+import { useRouter } from "next/router";
 export default function AuthLayout({ children }) {
+  
+  const router =Router();
+  const AuthContext = createContext();
+  
+    const { setAuth } = useContext(Context);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const auth = localStorage.getItem('token');
+    const main = new Listings();
+    const response = main.GetUserProfile();
+    response.then((res) => {
+      if (res.data.status) {
+        setAuth(res.data.data);
+      } else {
+        
+      }
+    }).catch((error) => {
+      console.log("error", error);
+      setTimeout(() => {
+        toast.error("Please log in first.");
+      }, 1000);
+      navigate('/'); 
+    });
+  }, []);
+
   return (
-    <>
-    <Header/>
+    <AuthContext.Provider value={{ auth }}>
+      <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </AuthContext.Provider>
   );
 }
--- pages/login/Login.js
+++ pages/login/Login.js
@@ -37,7 +37,8 @@ export default function Login() {
       if (res && res.data && res.data.status) {
         toast.success(res.data.message);
         // console.log("res",res)
-        setAuth(res?.data);
+   const record =      setAuth(res?.data);
+   console.log("record",record)
         localStorage && localStorage.setItem("token", res?.data?.token);
         router.push('/');
         // console.log(res.data.message)
