8773e042562a0634735d54ecf12241994d3f0758
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-20 09:53:21 +0530

    Api intewrgration

--- pages/home/Header.js
+++ pages/home/Header.js
@@ -5,7 +5,7 @@ import logo from "../../public/images/QsJaipur.png";
 import userprofile from "../../public/images/profile.png";
 
 export default function Header() {
-  // const token =localStorage.getItem("token")
+   const token ="111"
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
