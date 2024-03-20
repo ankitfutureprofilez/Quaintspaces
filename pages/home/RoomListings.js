0c3cf9a0d297dbe2f816ff4b81fdecfe3d6c9244
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-20 09:49:43 +0530

    Api intewrgration

--- pages/home/RoomListings.js
+++ pages/home/RoomListings.js
@@ -1,24 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { PostBody } from "../../components";
-import axios from "axios";
-import Listings from "../api/Listings";
+import Listings from './../api/laravel/Listings';
 
 export default function RoomListings() {
-  
+
   const [loading, setloading] = useState(false);
   const [listings, setListings] = useState([]);
   useEffect(() => {
     setloading(true);
-      const main = new Listings();
-      main.PropertyListing().then((r)=>{
-        setloading(false)
-        setListings(r.data.data);
-      }).catch((err)=>{
-        setloading(false);
-        console.log(err);
-      });
+    const main = new Listings();
+    main.PropertyListing().then((r) => {
+      setloading(false)
+      setListings(r.data.data);
+    }).catch((err) => {
+      setloading(false);
+      console.log(err);
+    });
   }, []);
   return (
-      <PostBody loading={loading} listings={listings} />
+    <PostBody loading={loading} listings={listings} />
   );
 }
--- pages/listings/[slug].js
+++ pages/listings/[slug].js
@@ -8,21 +8,21 @@ import { Context } from "../_app";
 import Wishlist from "../../components/Wishlist";
 import Layout from "../layout/Layout";
 import ThingsToKnow from "./ThingsToKnow";
-import Listings from "../LaravelApi/Listings";
+import Listings from './../api/laravel/Listings';
 
 const Listing = () => {
   const router = useRouter();
-  const {slug}= router.query;
+  const { slug } = router.query;
   const { wishlist, setWishlist } = useContext(Context);
   const [overlay, setOverlay] = useState(false);
   const [selection, setSelection] = useState(null);
   const [headerSearch, setHeaderSearch] = useState(false);
-  const[loading,setLoading]=useState(true);
+  const [loading, setLoading] = useState(true);
   const [listing, setListing] = useState({
     loading: true,
     data: {},
   });
-  
+
   useEffect(() => {
     if (slug) {
       setListing({
@@ -30,12 +30,12 @@ const Listing = () => {
         data: {},
       });
       const main = new Listings();
-      main.PropertyDetail(slug || "").then((r)=>{
-          setListing({
-            loading: false,
-            data: r.data.data,    
-          });
-      }).catch((err)=>{
+      main.PropertyDetail(slug || "").then((r) => {
+        setListing({
+          loading: false,
+          data: r.data.data,
+        });
+      }).catch((err) => {
         setListing({
           loading: true,
         });
@@ -46,14 +46,14 @@ const Listing = () => {
 
   return (
     <>
-    <Layout>
-      <Head>
-        <title>
-          House rent in {listing.loading ? "..." : listing.data?.title} - Aribnb
-          Clone
-        </title>
-      </Head>
-      {/* <Header
+      <Layout>
+        <Head>
+          <title>
+            House rent in {listing.loading ? "..." : listing.data?.title} - Aribnb
+            Clone
+          </title>
+        </Head>
+        {/* <Header
         header="relative"
         width="max-w-[1120px] hidden lg:flex"
         setOverlay={setOverlay}
@@ -62,20 +62,20 @@ const Listing = () => {
         headerSearch={headerSearch}
         setHeaderSearch={setHeaderSearch}
       /> */}
-      <SingleListingBody listing={listing} />
-      <ThingsToKnow/>
-      {/* <Footer /> */}
-      {overlay && (
-        <div
-          className="overlayFixed fixed top-0 left-0 w-full h-full z-10 bg-black bg-opacity-40"
-          onClick={() => {
-            setSelection(null);
-            setOverlay(false);
-            setHeaderSearch(false);
-          }}
-        ></div>
-      )}
-      {/* {wishlist && <Wishlist setWishlist={setWishlist} />} */}
+        <SingleListingBody listing={listing} />
+        <ThingsToKnow />
+        {/* <Footer /> */}
+        {overlay && (
+          <div
+            className="overlayFixed fixed top-0 left-0 w-full h-full z-10 bg-black bg-opacity-40"
+            onClick={() => {
+              setSelection(null);
+              setOverlay(false);
+              setHeaderSearch(false);
+            }}
+          ></div>
+        )}
+        {/* {wishlist && <Wishlist setWishlist={setWishlist} />} */}
       </Layout>
     </>
   );
