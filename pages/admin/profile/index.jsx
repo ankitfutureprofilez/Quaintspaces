8ebc735ab9d6aba7343c22484dc14b202ce056d2
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-26 18:35:39 +0530

    loaidng

--- pages/admin/profile/index.jsx
+++ pages/admin/profile/index.jsx
@@ -9,7 +9,6 @@ export default function Profileindex() {
     email: "",
     phone: "",
     name: "",
-    last: "",
     image: {},
   });
   const [previewImgSrc, setPreviewImgSrc] = useState("");
@@ -24,7 +23,6 @@ export default function Profileindex() {
         const profiledata = response.data.data;
         setRecord({
           name: profiledata.name,
-          last: profiledata.last_name,
           phone: profiledata.phone_no,
           email: profiledata.email,
           image: profiledata.image_url,
@@ -54,16 +52,12 @@ export default function Profileindex() {
       formdata.append("image", record.image);
       formdata.append("name", record.name);
       formdata.append("phone_no", record.phone);
-      
       const response = await main.AdminProfileUpdate(formdata);
       if (response.data.status) {
         toast.success(response.data.message);
-        // Update the state with new profile data
         setRecord((prevRecord) => ({
           ...prevRecord,
-          // Assuming response.data contains updated profile data
           name: response.data.name,
-          last: response.data.last_name,
           phone: response.data.phone_no,
           email: response.data.email,
           image: response.data.image_url,
@@ -90,7 +84,6 @@ export default function Profileindex() {
   return (
     <>
       <div className="container mx-auto mt-5">
-        {/* Image Upload */}
         <div className="flex items-center profile-border">
           <div className="relative">
             <div className="shrink-0">
@@ -99,27 +92,22 @@ export default function Profileindex() {
             <label className="block">
               <span className="sr-only">Choose profile photo</span>
               <input type="file" onChange={loadFile} className="hidden" />
-              {/* SVG Icon */}
               <div className='absolute top-0 right-0 p-1 bg-orange-300 rounded-full'>
                 <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                   <path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </div>
-
             </label>
           </div>
-
           <div className="ml-7 edit-here">
             <h2>Edit Profile</h2>
             <p>Update your profile here</p>
           </div>
           <div className='border-b-2 border-soild border-zinc-300 '> </div>
         </div>
-
       </div>
       <div className='container mx-auto mt-5 perso-form'>
-
         <div className="pers-info ">
           <h3 >Personal Information</h3>
           <p>Update your personal information here </p>
@@ -143,8 +131,6 @@ export default function Profileindex() {
                 required
               />
             </div>
-
-           
             <div className="mb-2 sm:mb-4">
               <label
                 htmlFor="email"
@@ -187,7 +173,6 @@ export default function Profileindex() {
         </div>
         <div className="border-bottom-form"></div>
       </div>
-
     </>
   );
 }
