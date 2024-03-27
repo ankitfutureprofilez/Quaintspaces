7541f7b32e69919aad76071a85f86144ec2c8d0b
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-27 10:34:27 +0530

    loaidng

--- pages/admin/integrations/index.jsx
+++ pages/admin/integrations/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Page from "./page"
-
+import AdminLayout from "../AdminLayout"
 export default function Index() {
   return (
     <div>
+      <AdminLayout>
       <Page/>
+      </AdminLayout>
     </div>
   );
 }
