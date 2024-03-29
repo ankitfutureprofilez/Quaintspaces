77727488c860df6e0d51a14c94434d9667d373d9
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-03-29 18:23:18 +0530

    ss

--- pages/admin/api/Listing.js
+++ pages/admin/api/Listing.js
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import Api from './../../api/laravel/Api';
+import Api  from "../ApiAdmin"
 class Listing extends Component {
     async adminlogin(data) {
         return Api.post("/admin/login",data);
