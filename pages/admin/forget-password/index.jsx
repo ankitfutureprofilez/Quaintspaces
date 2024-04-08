146e331da136b04e5a426fd4f2553f39184b0366
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-04-08 09:31:16 +0530

    merge

--- pages/admin/forget-password/index.jsx
+++ pages/admin/forget-password/index.jsx
@@ -114,6 +114,7 @@ export default function Index() {
                             {status ? (
                                 optStatus ? (
 <>
+
                                     <div className="space-y-5">
                                         <div className="space-y-2">
                                             <label className="text-sm font-medium text-gray-700 tracking-wide">Password </label>
