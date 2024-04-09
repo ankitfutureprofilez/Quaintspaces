f0460131140b7bb4d41b600758cee9c3d928d904
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-04-09 15:01:23 +0530

    payment success

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function index() {
+
   return (
     <div>
-         {images.map((reason, index) => (
+         {images && images.map((reason, index) => (
                 <div className="item flex-shrink-0 mx-2 relative transition-transform duration-500 ease-in-out transform" key={index}>
                   <Image src={reason.src} alt={reason.alt} className="" />
                   <div className="w-full py-4">
