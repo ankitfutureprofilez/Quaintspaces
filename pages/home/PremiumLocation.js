a9ceefc3146cf94327b16323e692bd45c1b2c402
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-04-17 14:55:43 +0530

    aa

--- pages/home/PremiumLocation.js
+++ pages/home/PremiumLocation.js
@@ -4,16 +4,11 @@ import Vaishali from "../../public/images/jhotwara-1.jpg";
 import Mansarovar from "../../public/images/jhotwara-2.jpg";
 import Jhotwara from "../../public/images/jhotwara-3.jpg";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'; 
-import 'swiper/css/navigation'; 
-import 'swiper/css/pagination'; 
-import 'swiper/css/scrollbar'; 
-// import SwiperCore, { Keyboard, Scrollbar, Navigation } from 'swiper';
-// Install Swiper modules
-// SwiperCore.use([Keyboard, Scrollbar, Navigation]);
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
 
 export default function PremiumLocation() {
-  const [swiper, setSwiper] = useState(null);
 
   const images = [
     { src: Vaishali, alt: 'Image 1', text: 'Vaishali Nagar' },
@@ -33,44 +28,26 @@ export default function PremiumLocation() {
     { src: Jhotwara, alt: 'Image 1', text: 'Malviya Nagar' },
   ];
 
-  const handleSwiper = (swiper) => {
-    setSwiper(swiper);
-  };
-
-  const slideNext = () => {
-    if (swiper) swiper.slideNext();
-  };
-
-  const slidePrev = () => {
-    if (swiper) swiper.slidePrev();
-  };
-
   return (
     <div className="premium-location-slider z-0" id="premium">
       <div className='container mx-auto '>
         <h1>Premium Location in Jaipur</h1>
         <div className="relative flex items-center ">
-          <Swiper
-            slidesPerView={1}
-            centeredSlides={false}
-            slidesPerGroupSkip={1}
-            grabCursor={true}
-            keyboard={{
-              enabled: true,
-            }}
-            breakpoints={{
-              769: {
-                slidesPerView: 3,
-                slidesPerGroup: 1,
-              },
-            }}
-            navigation={{
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            }}
-            className="mySwiper"
-            onSwiper={handleSwiper}
-          >
+        <Swiper navigation={true} modules={[Navigation]} 
+      slidesPerView={1}
+      centeredSlides={false}
+      slidesPerGroupSkip={1}
+      grabCursor={true}
+      keyboard={{
+        enabled: true,
+      }}
+      breakpoints={{
+        769: {
+          slidesPerView: 3,
+          slidesPerGroup: 1,
+        },
+      }}
+      className="mySwiper">
             {images.map((reason, index) => (
               <SwiperSlide key={index}>
                 <div className="item flex-shrink-0 mx-2 relative transition-transform duration-500 ease-in-out transform" key={index}>
@@ -82,18 +59,6 @@ export default function PremiumLocation() {
               </SwiperSlide>
             ))}
           </Swiper>
-          <button aria-label="next" name="next" title='next' onClick={slideNext} className="text-[0px] absolute -right-5 z-10 transform -translate-y-1/2 bg-gray-900 bg-opacity-50 text-white px-2 py-1 rounded-r">
-          <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M19.0941 13.5428H0.314991V10.4572L19.0941 10.4572L10.8185 2.18154L13 0L25 12L13 24L10.8185 21.8185L19.0941 13.5428Z" fill="white"/>
-        </svg>
-         Next Button 
-          </button>
-          <button aria-label="previous" name="previous" title='previous' className="text-[0px] absolute -left-5 z-10 transform -translate-y-1/2 bg-gray-900 bg-opacity-50 text-white px-2 py-1 rounded-r" onClick={slidePrev}>
-        <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5.90589 13.5428H24.685V10.4572L5.90589 10.4572L14.1815 2.18154L12 0L0 12L12 24L14.1815 21.8185L5.90589 13.5428Z" fill="white"/>
-        </svg>
-        Previous Button 
-          </button>
         </div>
       </div>
     </div>
---
+++ pages/home/server.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NextResponse } from 'next/server'
+
+export default function server() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
