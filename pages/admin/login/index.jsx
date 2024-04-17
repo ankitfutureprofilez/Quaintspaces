4c02eaf848c57b406cdea245b2e5b01375677b6c
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-04-17 15:43:36 +0530

    aa

--- pages/admin/login/index.jsx
+++ pages/admin/login/index.jsx
@@ -82,8 +82,8 @@ export default function Login() {
       <div className="flex justify-center self-center  z-10">
         <div className="p-12 bg-white mx-auto rounded-2xl w-100 ">
             <div className="mb-4">
-              <h3 className="font-semibold text-2xl text-gray-800">Sign In </h3>
-              <p className="text-gray-500">Please sign in to your account.</p>
+              <h3 className="font-semibold text-2xl text-black-800">Sign In </h3>
+              <p className="text-black-500">Please sign in to your account.</p>
             </div>
 
             <form onSubmit={handleSubmit}>
