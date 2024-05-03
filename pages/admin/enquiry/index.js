229b5d6a4ca6ef0a5df343ee0878f9ff2dbdb365
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-05-03 18:19:38 +0530

    aa

--- pages/admin/enquiry/index.js
+++ pages/admin/enquiry/index.js
@@ -93,6 +93,7 @@ export default function Index() {
                         >
                           Reply Message
                         </th>
+                    
                         <th
                           scope="col"
                           className="min-w-50 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
