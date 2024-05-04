9266bce388df420d86dd366290771a5e34117c6e
unknown <ankit.jain0748@gmail.com>
2024-05-04 14:49:00 +0530

    ss

--- components/SingleListingBody/Title.jsx
+++ components/SingleListingBody/Title.jsx
@@ -10,6 +10,9 @@ const Title = ({ isSaved, listing, addWishlist,loading }) => {
     const words =str && str?.split(" ");
     
     
+
+
+    
     // Capitalize the first letter of each word
     const capitalizedWords = words?.map(word => word?.charAt(0)?.toUpperCase() + word?.slice(1));
   
