81d7d1b12b3be40c2299f8cdfb92cb11014b6a1f
Abhinav-FP <a.mathur@futureprofilez.com>
2024-05-20 18:21:06 +0530

    Added drag and drop map

--- pages/test.js
+++ pages/test.js
@@ -1,30 +1,65 @@
-import React from 'react';
-import StartRating from './elements/StartRating';
+import React, { useState, useCallback } from "react";
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from "@react-google-maps/api";
 
-    const Users = ({ users }) => {
-      return (
-        <div>
-          <h1>Users</h1>
-          <ul>
-            {/* {users.map((user) => (
-              <li key={user.id}>{user.name}</li>
-            ))} */}
+const containerStyle = {
+  width: "100%",
+  height: "400px",
+};
 
-          </ul>
-            <StartRating size={4} value={"3"} />
-        </div>
-      );
+const center = {
+  lat: 37.39094933041195,
+  lng: -122.02503913145092,
+};
+
+const MapComponent = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY,
+  });
+
+  const [markerPosition, setMarkerPosition] = useState(center);
+  const [infoWindowPosition, setInfoWindowPosition] = useState(null);
+  const [infoWindowContent, setInfoWindowContent] = useState("");
+
+  const onMarkerDragEnd = useCallback((event) => {
+    const newPosition = {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
     };
+    setMarkerPosition(newPosition);
+    setInfoWindowPosition(newPosition);
+    setInfoWindowContent(`Pin dropped at: ${newPosition.lat}, ${newPosition.lng}`);
+  }, []);
 
-    // export async function getServerSideProps() {
-    //   // Fetch data from jsonplaceholder API 
-    //   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    //   const users = await response.json();
+  return (
+    <div>
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={14}
+          mapId="4504f8b37365c3d0"
+        >
+          <Marker
+            position={markerPosition}
+            draggable={true}
+            onDragEnd={onMarkerDragEnd}
+            title="This marker is draggable."
+          />
+          {infoWindowPosition && (
+            <InfoWindow
+              position={infoWindowPosition}
+              onCloseClick={() => setInfoWindowPosition(null)}
+            >
+              <div>{infoWindowContent}</div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
+};
 
-    //   return {
-    //     props: {
-    //       users,
-    //     },
-    //   };
-    // }    
-    export default Users;
+export default MapComponent;
