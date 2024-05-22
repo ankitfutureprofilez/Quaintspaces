c66159a3951b2b05f3aa35a482a48adb3d09c17c
Abhinav-FP <a.mathur@futureprofilez.com>
2024-05-22 18:33:22 +0530

    Form and pricve saved in test

--- pages/test.js
+++ pages/test.js
@@ -1,65 +1,88 @@
-import React, { useState, useCallback } from "react";
-import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from "@react-google-maps/api";
+import React, { useState } from "react";
 
-const containerStyle = {
-  width: "100%",
-  height: "400px",
-};
+export default function test() {
 
-const center = {
-  lat: 37.39094933041195,
-  lng: -122.02503913145092,
-};
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setItem({ ...item, [name]: value });
+  };
 
-const MapComponent = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY,
+  const [propertyDuplicated, setpropertyDuplicated] = useState(false);
+  const [item, setItem] = useState({
+    cleaning: "",
+    pet:"",
+    guest:"",
   });
 
-  const [markerPosition, setMarkerPosition] = useState(center);
-  const [infoWindowPosition, setInfoWindowPosition] = useState(null);
-  const [infoWindowContent, setInfoWindowContent] = useState("");
-
-  const onMarkerDragEnd = useCallback((event) => {
-    const newPosition = {
-      lat: event.latLng.lat(),
-      lng: event.latLng.lng(),
-    };
-    setMarkerPosition(newPosition);
-    setInfoWindowPosition(newPosition);
-    setInfoWindowContent(`Pin dropped at: ${newPosition.lat}, ${newPosition.lng}`);
-  }, []);
-
   return (
-    <div>
-      {isLoaded ? (
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={14}
-          mapId="4504f8b37365c3d0"
-        >
-          <Marker
-            position={markerPosition}
-            draggable={true}
-            onDragEnd={onMarkerDragEnd}
-            title="This marker is draggable."
-          />
-          {infoWindowPosition && (
-            <InfoWindow
-              position={infoWindowPosition}
-              onCloseClick={() => setInfoWindowPosition(null)}
-            >
-              <div>{infoWindowContent}</div>
-            </InfoWindow>
-          )}
-        </GoogleMap>
+    <>
+    <div className="mt-32 flex flex-col items-center space-y-12">
+      <h1 className="mb-8 capitalize font-bold text-2xl">
+        {" "}
+        Please select an option
+      </h1>
+      {!propertyDuplicated ? (
+        <>
+          <button className="border-gray border-2 px-8 py-8 rounded-full w-3/5 capitalize">
+            Add New Property
+          </button>
+          <button 
+          onClick={()=>{setpropertyDuplicated(true);}}
+          className="border-gray border-2 px-8 py-8 rounded-full w-3/5 capitalize">
+            Duplicate Existing Property
+          </button>
+        </>
       ) : (
-        <div>Loading...</div>
+        <>
+          <button className="border-gray border-2 px-8 py-8 rounded-full w-3/5 capitalize">
+            Edit Entire Propertty
+          </button>
+          <button className="border-gray border-2 px-8 py-8 rounded-full w-3/5 capitalize">
+            Only edit Image
+          </button>
+        </>
       )}
     </div>
-  );
-};
+    {/* Form Code Starts */}
+    <div className={`max-w-[100%] m-auto table w-full mt-10`}>
+              <h2 className="text-3xl text-center font-bold mb-8 capitalize">Please enter the following details?</h2>
+              <div className="flex flex-col mt-4 text-sm font-medium text-gray-700">
+                <label>Cleaning Fees</label>
+                <input
+                  required
+                  type="number"
+                  name="cleaning" placeholder="Cleaning Fees Per Day"
+                  id="cleaning"
+                  className="mt-1 p-3 px-4 focus:outline-0 border rounded-xl w-3/5"
+                  value={item?.cleaning}
+                  onChange={handleInputChange}
+                />
+              </div>
 
-export default MapComponent;
+              <div className="flex flex-col relative mt-4 text-sm font-medium text-gray-700">
+              <label>Pet Fees</label>
+                <input
+                  type="number"
+                  name="pet" placeholder="Pet Fees"
+                  id="pet"
+                  className="mt-1 p-3 px-4 focus:outline-0 border rounded-xl w-3/5"
+                  value={item?.pet}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="flex flex-col relative mt-4 text-sm font-medium text-gray-700">
+              <label>Extra Guest Fees (Per Guest)</label>
+                <input
+                  required
+                  type="number"
+                  name="guest" placeholder="Extra Guest Fees"
+                  id="guest"
+                  className="mt-1 p-3 px-4 focus:outline-0 border rounded-xl w-3/5"
+                  value={item?.guest}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </div>
+    </>
+  );
+}
