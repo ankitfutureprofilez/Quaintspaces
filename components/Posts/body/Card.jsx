5b09f28f87c8f369b4fe6ddd6eabae54dda8e574
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-05-31 18:23:48 +0530

    property

--- components/Posts/body/Card.jsx
+++ components/Posts/body/Card.jsx
@@ -44,6 +44,8 @@ const Card = ({ post }) => {
           src={post?.property_image[0]?.image_url}
           alt="Property cover image"
         />
+
+        
         <div className="flat-info">
           <h2 className="line-limit sm:min-h-[72px]">
             {/* {textResizer(
