9f60d47f0a286ac29a40089ff7cab475acba8225
Abhinav-FP <a.mathur@futureprofilez.com>
2024-07-02 12:55:22 +0530

    Star

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"
         ></link>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.jpeg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
@@ -22,7 +22,7 @@ export default function Document() {
       </Head>
       <body>
         <Main />
-        <NextScript />
+        <NextScript /> 
       </body>
     </Html>
   );
