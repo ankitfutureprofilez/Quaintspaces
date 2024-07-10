07828673ee6897d2c9f949722692303d79a94e54
Abhinav Mathur <mathur.abhinav1108@gmail.com>
2024-07-10 11:09:29 +0530

    Footer Done

--- pages/home/Footer.js
+++ pages/home/Footer.js
@@ -6,56 +6,59 @@ import { FaFacebook, FaInstagram } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-      <div className="container mx-auto flex flex-col justify-between">
-        {/* Logo and Brand */}
-        <div className="footer-menu items-end">
-          <div className="">
-            <h3 className="menu-footer mb-3">CONTACT US</h3>
-            <div className="flex flex-col">
+    <div className="container mx-auto flex flex-col justify-between">
+      {/* Logo and Brand */}
+      <div className="footer-menu items-end">
+        <div className="">
+          <h3 className="menu-footer mb-3">CONTACT US</h3>
+          <div className="flex flex-col">
             <Link href="mailto:quaintspaces@gmail.com" target="_blank">
-            <span className="capiatize">Email : </span>
-            quaintspaces@gmail.com</Link>
+              <span className="capiatize">Email : </span>
+              quaintspaces@gmail.com
+            </Link>
             <Link href="tel:9521410122">
-            <span className="capiatize">Mobile : </span>
-            9521410122
+              <span className="capiatize">Mobile : </span>
+              9521410122
+            </Link>
+          </div>
+          {/* <p><Link href="tel:9314022666">9314022666</Link></p> */}
+        </div>
+        <div className="">
+          <h3 className="menu-footer mb-3"></h3>
+          <div className="flex flex-col">
+            <Link href="/terms">
+              <span className="capiatize">Terms & Condition</span>
+            </Link>
+            <Link href="/policy">
+              <span className="capiatize">Privacy Policy</span>
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <h3 className="mb-2">Follow Us</h3>
+          <div className="">
+            <Link href="/">
+              <div className="flex">
+                <FaFacebook color={"#c48b58"} size={24} />
+                <span>: Follow us on Facebook</span>
+              </div>
             </Link>
             </div>
-            {/* <p><Link href="tel:9314022666">9314022666</Link></p> */}
-
-          </div>
-          <div className="mt-2">
-            <p className="menu-footer ">
-              <Link href="/terms">
-                TERMS & CONDITION
-              </Link></p> <p className="menu-footer">
-              <Link href="/policy">
-                PRIVACY POLICY
-              </Link></p>
-          </div>
-          <div className="">
-  <h3 className="mb-3">Follow Us</h3>
-  <div className="">
-    <Link href="/">
-      <div className="flex">
-        <FaFacebook color={"#c48b58"} size={28} />
-        <span>: Follow us on Facebook</span>
-      </div>
-    </Link>
-    <Link href="https://www.instagram.com/quaintspacesjaipur">
-      <div className="flex">
-        <FaInstagram color={"#c48b58"} size={28} />
-        <span>: @quaintspacesjaipur</span>
-      </div>
-    </Link>
-  </div>
-</div>
-
-        </div>
-
-        <div className="text-center copy-right">
-          {/* Copyright Notice */}
-          <span>&copy; QUAINTSPACES JAIPUR 2024</span>
+            <div className="mt-2">
+            <Link href="https://www.instagram.com/quaintspacesjaipur">
+              <div className="flex">
+                <FaInstagram color={"#c48b58"} size={24} />
+                <span>: @quaintspacesjaipur</span>
+              </div>
+            </Link>
+            </div>          
         </div>
       </div>
+
+      <div className="text-center copy-right">
+        {/* Copyright Notice */}
+        <span>&copy; QUAINTSPACES JAIPUR 2024</span>
+      </div>
+    </div>
   );
 }
