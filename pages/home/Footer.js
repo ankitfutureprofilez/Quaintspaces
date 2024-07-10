3b2c071febbed08e520aa8ffe1ee3baca0a249e8
ankitjain0748 <ankitjain0748@gmail.com>
2024-07-10 16:35:33 +0530

    lsiitng chnage  emtn

--- pages/home/Footer.js
+++ pages/home/Footer.js
@@ -5,30 +5,30 @@ import Link from "next/link";
 import { FaFacebook, FaInstagram } from "react-icons/fa6";
 
 export default function Footer() {
-  
   return (
     <div className="container mx-auto flex flex-col justify-between">
       {/* Logo and Brand */}
-      <div className="footer-menu items-end">
-        <div className="">
-          <h3 className="mb-3 text-[20px] text[#3F2A17] font-[400] font-['Baskervville']">CONTACT US</h3>
+      <div className="items-start footer-menu ">
+        <div className="w-full mb-3 mb:mb-0">
+          <h3 className="mb-2 text-[20px] text[#3F2A17] font-[400] font-['Baskervville']">CONTACT US</h3>
           <div className="flex flex-col">
             <Link className="text-[#3F2A17] text-[16px] " href="mailto:quaintspaces@gmail.com" target="_blank">
               <span >Email : </span>
               quaintspaces@gmail.com
             </Link>
-            <Link href="tel:9521410122">
-              <span className="capiatize">Mobile : </span>
+            <Link className="text-[#3F2A17] text-[16px] uppercase" href="tel:9521410122">
+              <span >Mobile : </span>
               9521410122
             </Link>
-          <Link href="tel:9314022666">
-              <span className="capiatize">Mobile : </span>
-          9314022666</Link>
+            <Link className="text-[#3F2A17] text-[16px] uppercase" href="tel:9521410122">
+              <span >Mobile : </span>
+              9521410122
+            </Link>
           </div>
-          
+          {/* <p><Link href="tel:9314022666">9314022666</Link></p> */}
         </div>
-        <div className="">
-        <h3 className="uppercase mb-3 text-[20px] text[#3F2A17] font-[400] font-['Baskervville']">Quick Links</h3>
+        <div className="w-full mb-3 mb:mb-0">
+        <h3 className="uppercase mb-2 text-[20px] text[#3F2A17] font-[400] font-['Baskervville']">Quick Links</h3>
           <div className="flex flex-col">
             <Link href="/terms">
               <span className="text-[#3F2A17] text-[16px] uppercase">Terms & Condition</span>
@@ -41,18 +41,18 @@ export default function Footer() {
         <div className="flex w-full flex-col mb-3 mb:mb-0">
           <h3 className="uppercase mb-2 text-[20px] text[#3F2A17] font-[400] font-['Baskervville']">Follow Us</h3>
           <div className="">
-            <Link href="https://www.facebook.com/share/64gvciqN2UNPMx6V/?mibextid=LQQJ4d">
+            <Link href="/">
               <div className="flex">
-                <FaFacebook color={"#c48b58"} size={24} />
-                <span>: Follow us on Facebook</span>
+                <FaFacebook color={"#3F2A17"} size={24} />
+                <span className="text-[#3F2A17] text-[16px] uppercase ml-2">: Follow us on Facebook</span>
               </div>
             </Link>
             </div>
             <div className="mt-2">
             <Link href="https://www.instagram.com/quaintspacesjaipur">
               <div className="flex">
-                <FaInstagram color={"#c48b58"} size={24} />
-                <span>: @quaintspacesjaipur</span>
+                <FaInstagram color={"#3F2A17"} size={24} />
+                <span className="text-[#3F2A17] text-[16px] uppercase ml-2">: @quaintspacesjaipur</span>
               </div>
             </Link>
             </div>          
