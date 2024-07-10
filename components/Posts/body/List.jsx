d6ad2fa6b714e7c7f61a4d92d986e441cb29edfa
fp-gajendra <gajendra@futureprofilez.comgit  config --global user.name fp-gajendra>
2024-07-10 11:57:52 +0530

    font

--- components/Posts/body/List.jsx
+++ components/Posts/body/List.jsx
@@ -38,7 +38,7 @@ function List({ post }) {
         <Link className="block relative  sm:pb-[20px] h-full" href={`/property/${post?.uuid}`}>
 
           <div className="bg-white flex-wrap rounded-lg overflow-hidden relative list-gstr overflow-hidden flex">
-            <div className="w-full lg:w-4/12 max-h-[300px]">
+            <div className="w-full lg:w-3/12 max-h-[300px]">
               {post?.discount_offer ? (
                 <div className="absolute bg-[#e0c4c3] -rotate-45 text-white px-2 py-1 w-32 text-center -left-[32px] top-[18px] shadow-[0_0_17px_-5px_#3c3c3c;]">
                   {post?.discount_offer}%  off
@@ -50,14 +50,14 @@ function List({ post }) {
                 className="w-full h-full object-cover object-center"
               />
             </div>
-            <div className="w-full lg:w-8/12 p-3 sm:p-6">
+            <div className="w-full lg:w-9/12 p-3 sm:p-6">
               <div className="flex flex-wrap justify-between">
                 <div className="w-full sm:w-8/12">
                   <h2 className="text-[22px] font-[600] text-[#3F2A17] mb-[10px]">  {capitalizeFirstLetter(post?.name)}</h2>
 
                   <p className="text-[16px] font-[400] text-[#666360] uppercase sm:mb-[15px] mb-[8px]"> {record?.location}</p>
                 </div>
-                <div className="w-full sm:w-4/12 mb-4 sm:mb-0">
+                <div className="w-full sm:w-4/12 mb-4 sm:mb-0 flex sm:justify-end">
                   <div>
                     <p className="text-[13px] font-[400] text-[#666360]">From</p>
                     <h2 className="text-[23px] sm:text-[30px] font-[600] text-[#E0C4C3] ">
@@ -73,20 +73,20 @@ function List({ post }) {
               </div> */}
               <div className="flex mb-[15px]">
 
-                <div className=" mb-2 sm:pr-[20px] pr-[10px]">
+                <div className=" mb-2 sm:pr-[20px] pr-[7px]">
                   <h3 className="text-[#666360] font-[400] text-[13px]">Bedrooms</h3>
                   <p className="text-[#666360] font-[700] text-[16px]"> {post?.bedrooms} </p>
                 </div>
 
-                <div className=" mb-2 sm:pr-[20px] pr-[10px] pl-[10px] sm:ml-[20px] ml-[10px] border-l">
+                <div className=" mb-2 sm:pr-[20px] pr-[7px] pl-[7px] sm:ml-[20px] ml-[7px] border-l">
                   <h3 className="text-[#666360] font-[400] text-[13px]">Bed</h3>
                   <p className="text-[#666360] font-[700] text-[16px]">{post?.beds}  </p>
                 </div>
-                <div className=" mb-2 sm:pr-[20px] pr-[10px] pl-[10px] sm:ml-[20px] ml-[10px] border-l">
+                <div className=" mb-2 sm:pr-[20px] pr-[7px] pl-[7px] sm:ml-[20px] ml-[7px] border-l">
                   <h3 className="text-[#666360] font-[400] text-[13px]">Guests</h3>
                   <p className="text-[#666360] font-[700] text-[16px]">{post?.guests} </p>
                 </div>
-                <div className=" mb-2 sm:pr-[20px] pr-[10px] pl-[10px] sm:ml-[20px] ml-[10px] border-l">
+                <div className=" mb-2 sm:pr-[20px] pr-[7px] pl-[7px] sm:ml-[20px] ml-[7px] border-l">
                   <h3 className="text-[#666360] font-[400] text-[13px]">Pets</h3>
                   <p className="text-[#666360] font-[700] text-[16px]">{post?.no_of_pet_allowed} </p>
                 </div>
