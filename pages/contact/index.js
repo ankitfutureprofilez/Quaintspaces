e65ba557e9637a1891ece725ed599675dbf49058
fp-gajendra <gajendra@futureprofilez.comgit  config --global user.name fp-gajendra>
2024-07-12 11:53:37 +0530

    about us page done responsive done

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -75,7 +75,7 @@ export default function index() {
       {/* Hero sec component */}
       <div className="bg-cover bg-no-repeat bg-center contact-about" style={{ backgroundImage: `url(/images/banner/Banner1.JPG)` }} >
 
-        <div className="py-[200px] bg-[#00000087]">
+        <div className="lg:py-[200px] md:py-[150px] sm:py-[100px] py-[50px] bg-[#00000087]">
           <div className="">
             <h1 className="">Discover the Art of Luxurious Living</h1>
             <p className=" max-w-[800px] m-auto">
@@ -86,69 +86,73 @@ export default function index() {
         </div>
       </div>
       {/* Why Book with us */}
-      <div className="max-w-6xl mx-auto py-[100px]">
-        <h2 className="mb-4 text-[40px] text[#3F2A17] text-center font-[400] font-['Baskervville']">Why Book With Us</h2>
-        <div className="grid md:grid-cols-3 gap-4">
-          <div className="bg-[#efa3a3] py-[50px] px-[40px] flex flex-col items-start ">
-            <h3 className="text-[24px] text-[#3F2A17] mb-[15px]">Unmatched Quality</h3>
-            <p className="text-[16px] text-[#3F2A17]">Each property is meticulously selected to ensure the highest standards of luxury and comfort.</p>
-          </div>
-          <div className=""  style={{ backgroundImage: `url(/images/Book1.jpg)` }} >
-            <div className="py-[50px] bg-[#00000073] h-full px-[40px] flex flex-col items-start">
-                <h3 className="text-[24px] text-[#fff] mb-[15px]">Personalized Experience</h3>
-                <p className="text-[16px] text-[#fff]">Enjoy tailored services designed to meet your unique preferences and needs.</p>
+      <div className="  lg:py-[100px] md:py-[80px] sm:py-[50px] py-[30px]">
+        <div className="container mx-auto">
+          <h2 className="sm:mb-4 mb-2 text-[26px] sm:text-[30px] md:text-[40px] text[#3F2A17] text-center font-[400] font-['Baskervville']">Why Book With Us</h2>
+          <div className="grid md:grid-cols-3 gap-4">
+            <div className="bg-[#efa3a3] py-[30px] lg:py-[50px] px-[20px] lg:px-[40px] flex flex-col items-start ">
+              <h3 className="text-[20px] sm:text-[24px] text-[#3F2A17] mb-[8px] sm:mb-[15px]">Unmatched Quality</h3>
+              <p className="text-[16px] text-[#3F2A17]">Each property is meticulously selected to ensure the highest standards of luxury and comfort.</p>
             </div>
-          </div>
-          <div>
-            <div   style={{ backgroundImage: `url(/images/Book.jpg)` }}>
-              <div className="py-[50px] bg-[#00000073] h-full px-[40px] mb-4 flex flex-col items-start">
-                  <h3 className="text-[24px] text-[#fff] mb-[15px]">Seamless Booking</h3>
-                  <p className="text-[16px] text-[#fff]">Our user-friendly platform makes finding and booking your perfect stay effortless.</p>
+            <div className=""  style={{ backgroundImage: `url(/images/Book1.jpg)` }} >
+              <div className="bg-[#00000073] h-full py-[30px] lg:py-[50px] px-[20px] lg:px-[40px] flex flex-col items-start">
+                  <h3 className="text-[20px] sm:text-[24px] text-[#fff] mb-[8px] sm:mb-[15px]">Personalized Experience</h3>
+                  <p className="text-[16px] text-[#fff]">Enjoy tailored services designed to meet your unique preferences and needs.</p>
               </div>
             </div>
-            <div className="bg-[#B1BBC2] py-[50px] px-[40px] flex flex-col items-start">
-              <h3 className="text-[24px] text-[#3F2A17] mb-[15px]">24/7 Support</h3>
-              <p className="text-[16px] text-[#3F2A17]">Our dedicated team is available around the clock to assist with any inquiries or requests.</p>
+            <div>
+              <div   style={{ backgroundImage: `url(/images/Book.jpg)` }}>
+                <div className=" bg-[#00000073] h-full py-[30px] lg:py-[50px] px-[20px] lg:px-[40px] flex flex-col items-start">
+                    <h3 className="text-[20px] sm:text-[24px] text-[#fff] mb-[8px] sm:mb-[15px]">Seamless Booking</h3>
+                    <p className="text-[16px] text-[#fff]">Our user-friendly platform makes finding and booking your perfect stay effortless.</p>
+                </div>
+              </div>
+              <div className="bg-[#B1BBC2] mt-4 md:mt-0 py-[30px] lg:py-[50px] px-[20px] lg:px-[40px] flex flex-col items-start">
+                <h3 className="text-[20px] sm:text-[24px] text-[#3F2A17] mb-[8px] sm:mb-[15px]">24/7 Support</h3>
+                <p className="text-[16px] text-[#3F2A17]">Our dedicated team is available around the clock to assist with any inquiries or requests.</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
       {/* Easy Steps */}
-      <div className="bg-[#E0E0B0] py-[100px]">
-        <h2 className="mb-4 text-[40px] text[#3F2A17] text-center font-[400] mb-[50px] font-['Baskervville']">Book Your Home In 3 Easy Steps</h2>
-        <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center md:items-start">
-          <div className="md:w-2/5 px-6">
-            <div className="space-y-6">
-              <div>
-                <h3 className="text-[24px] text-[#3F2A17] mb-[10px]"><span className="text-[34px] font-['Baskervville']">01</span> Explore Our Listings</h3>
-                <p className="text-[16px] text-[#666360] ">Browse through our curated selection of luxurious properties to find your perfect stay.</p>
-              </div>
-              <div>
-                <h3 className="text-[24px] text-[#3F2A17] mb-[10px]"><span className="text-[34px] font-['Baskervville']">02</span> Check Availability</h3>
-                <p className="text-[16px] text-[#666360]">Easily check availability and choose the dates that best suit your travel plans.</p>
-              </div>
-              <div>
-                <h3 className="text-[24px] text-[#3F2A17] mb-[10px]"><span className="text-[30px] font-['Baskervville']">03</span> Secure Your Booking</h3>
-                <p className="text-[16px] text-[#666360]">Complete your reservation with our seamless and secure booking process.</p>
+      <div className="bg-[#E0E0B0] lg:py-[100px] md:py-[80px] sm:py-[50px] py-[30px]">
+        <div className="container mx-auto">
+          <h2 className="sm:mb-6 mb-4  text-[26px] sm:text-[30px] md:text-[40px] text[#3F2A17] text-center font-[400] font-['Baskervville'] ">Book Your Home In 3 Easy Steps</h2>
+          <div className=" flex flex-col md:flex-row items-center md:items-start">
+            <div className="md:w-2/5 sm:px-6">
+              <div className="space-y-6">
+                <div>
+                  <h3 className="md:text-[24px] text-[20px] text-[#3F2A17] md:mb-[10px]"><span className="md:text-[34px] text-[24px] font-['Baskervville']">01</span> Explore Our Listings</h3>
+                  <p className="text-[16px] text-[#666360] ">Browse through our curated selection of luxurious properties to find your perfect stay.</p>
+                </div>
+                <div>
+                  <h3 className="md:text-[24px] text-[20px] text-[#3F2A17] md:mb-[10px]"><span className="md:text-[34px] text-[24px] font-['Baskervville']">02</span> Check Availability</h3>
+                  <p className="text-[16px] text-[#666360]">Easily check availability and choose the dates that best suit your travel plans.</p>
+                </div>
+                <div>
+                  <h3 className="md:text-[24px] text-[20px] text-[#3F2A17] md:mb-[10px]"><span className="md:text-[34px] text-[24px] font-['Baskervville']">03</span> Secure Your Booking</h3>
+                  <p className="text-[16px] text-[#666360]">Complete your reservation with our seamless and secure booking process.</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="md:w-3/5 px-6 mt-8 md:mt-0">
-            <div className="flex flex-col space-y-4 relative">
-              <div className="max-w-[380px] border-[6px] absolute top-0 left-0 border-[#e0e0b0] ">
-              <img src="/images/EasySteps2.JPG" alt="Modern living space" className="w-full" />
+            <div className="md:w-3/5 sm:px-6 mt-8 md:mt-0">
+              <div className="flex flex-col space-y-4 relative">
+                <div className="xl:max-w-[380px] lg:max-w-[320px] sm:max-w-[250px] max-w-[200px] border-[6px] absolute top-0 left-0 border-[#e0e0b0] ">
+                <img src="/images/EasySteps2.JPG" alt="Modern living space" className="w-full" />
+                </div>
+                <img src="/images/EasySteps1.jpg" alt="Luxurious living room" className=" ml-[100px] !mt-[159px]" />
               </div>
-              <img src="/images/EasySteps1.jpg" alt="Luxurious living room" className=" ml-[100px] !mt-[159px]" />
-            </div>
 
+            </div>
           </div>
         </div>
       </div>
       {/* Old Code */}
-      <div className="bg-[#f7f8f9] py-[80px]">
+      <div className="bg-[#f7f8f9] lg:py-[80px] md:py-[60px] sm:py-[40px] py-[25px]">
         <div className="container flex mx-auto w-full flex-wrap md:flex-nowrap space-x-7 mt-10">
           <div className="md:w-6/12 w-full">
-            <h2 className="text-[#3F2A17] text-[32px] border-b pb-[30px] mb-[50px]">
+            <h2 className="text-[#3F2A17] md:text-[32px] sm:text-[26px] text-[24px] border-b lg:pb-[30px] sm:pb-[20p] pb-[15] lg:mb-[50px] sm:mb-[40] mb-[20px]">
               Company info
             </h2>
             <div>
@@ -157,7 +161,7 @@ export default function index() {
                 <p className="capitalize text-[15px] text-[#7A746E] mb-[10px]">
                 {item?.key}
                 </p>
-                <h3 className="text-[24px] text-[#3F2A17] mb-[35px]"
+                <h3 className="md:text-[24px] text-[20px] text-[#3F2A17] md:mb-[35px] sm:mb-[28px] mb-[20px]"
                   >
               {item?.value}
                 </h3>
@@ -169,7 +173,7 @@ export default function index() {
 
           </div>
           <div className="md:w-6/12 w-full !ml-0 md:!ml-8 mt-4 md:mt-0">
-            <h2 className="text-[#3F2A17] text-[32px] border-b pb-[30px] mb-[57px]">
+            <h2 className="text-[#3F2A17] md:text-[32px] sm:text-[26px] text-[24px] border-b lg:pb-[30px] sm:pb-[20p] pb-[15] lg:mb-[50px] sm:mb-[40] mb-[20px]">
               Write to Us
             </h2>
             <div className="w-full security-box-form">
@@ -177,7 +181,7 @@ export default function index() {
                 <div className="mb-3 lg:mb-6">
                   <label
                     htmlFor="name"
-                    className="capitalize text-[15px] text-[#7A746E] mb-[10px]"
+                    className="capitalize text-[15px] text-[#7A746E] sm:mb-[10px] mb-[8px]"
                   >
                     Your Name
                   </label>
@@ -187,14 +191,14 @@ export default function index() {
                     name="name"
                     value={formData?.name}
                     onChange={handleChange}
-                    className="p-[16px] mt-[10px] rounded-full w-full"
+                    className="sm:p-[16px] p-[12px]  mt-[10px] rounded-full w-full"
                     required
                   />
                 </div>
                 <div className="mb-3 lg:mb-6">
                   <label
                     htmlFor="email"
-                    className="capitalize text-[15px] text-[#7A746E] mb-[10px]"
+                    className="capitalize text-[15px] text-[#7A746E] sm:mb-[10px] mb-[8px]"
                   >
                     Your Email
                   </label>
@@ -204,14 +208,14 @@ export default function index() {
                     name="email"
                     value={formData?.email}
                     onChange={handleChange}
-                    className="p-[16px] mt-[10px] rounded-full w-full"
+                    className="sm:p-[16px] p-[12px]   mt-[10px] rounded-full w-full"
                     required
                   />
                 </div>
                 <div className="mb-3 lg:mb-6">
                   <label
                     htmlFor="message"
-                    className="capitalize text-[15px] text-[#7A746E] mb-[10px]"
+                    className="capitalize text-[15px] text-[#7A746E] sm:mb-[10px] mb-[8px]"
                   >
                     Message
                   </label>
@@ -220,7 +224,7 @@ export default function index() {
                     name="message"
                     value={formData?.message}
                     onChange={handleChange}
-                    className="p-[16px] mt-[10px] rounded-3xl min-h-32 lg:min-h-52  w-full"
+                    className="sm:p-[16px] p-[12px]  mt-[10px] rounded-3xl min-h-32 lg:min-h-52  w-full"
                     required
                     rows={4} // Set the number of rows as needed
                   />
