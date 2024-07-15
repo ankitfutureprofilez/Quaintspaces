73a1810c6889f608839935d86af563106a9025f7
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-07-15 12:12:24 +0530

    merge

--- pages/home/Menu.js
+++ pages/home/Menu.js
@@ -14,7 +14,7 @@ export default function Menu() {
     localStorage && localStorage.removeItem("token");
     setAuth(null);
     toast.success("Logout Successfully !!");
-    router.push("/");
+    router.push("/login");
   };
 
   return (
@@ -127,30 +127,6 @@ export default function Menu() {
                 </svg>
                 <span>Settings</span>
               </Link>
-
-              {/* <Link
-                href="/contact"
-                className="flex items-center leading-6 space-x-3 py-3 px-4 w-full text-lg text-gray-600 focus:outline-none hover:bg-gray-100 rounded-md"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                  className="w-7 h-7"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M19.875 6.27c.7 .398 1.13 1.143 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z"></path>
-                  <path d="M12 16v.01"></path>
-                  <path d="M12 13a2 2 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483"></path>
-                </svg>
-                <span>Helper Center</span>
-              </Link> */}
             </nav>
           </div>
 
