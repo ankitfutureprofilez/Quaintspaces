3744239222a957d12aedb38be8448e8f87c4d351
Bipin <bipinkr7742@gmail.com>
2024-07-23 18:28:09 +0530

    Image

--- pages/signup/Signup.js
+++ pages/signup/Signup.js
@@ -60,9 +60,19 @@ export default function Signup() {
   return (
     <div
       className="h-screen tab-mob-height bg-cover "
-      style={{ backgroundImage: `url(/images/banner/login_img.JPG)` }}
+      // style={{ backgroundImage: `url(/images/banner/login_img.JPG)` }}
 
     >
+      <Image
+        src="/images/banner/login_img.JPG"
+        blurDataURL="/images/banner/login_img.JPG?q=1"
+        placeholder="blur"
+        alt="Login Background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="z-[-1]"
+      />
       <div className="container h-full">
         <div className="flex items-center  h-full relative signup-tab-sec">
           <div className="left-logo-login w-6/12 px-3">
