6648874113c0472c80d18ad5d44e56a42f6c6d0f
Bipin <bipinkr7742@gmail.com>
2024-07-23 14:54:38 +0530

    Footer Done

--- pages/home/Footer.js
+++ pages/home/Footer.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { FaFacebook, FaInstagram } from "react-icons/fa6";
 import { MdOutlineMail } from "react-icons/md";
 import { MdOutlinePhone } from "react-icons/md";
+import { TfiEmail } from "react-icons/tfi";
 
 export default function Footer() {
   return (
@@ -14,7 +15,7 @@ export default function Footer() {
           <h3 className="mb-2 text-[20px] text[#3F2A17] font-[400] font-['Baskervville'] capitalize">Contact Us</h3>
           <div className="flex flex-col">
             <Link className="text-[#3F2A17] text-[16px] flex " href="mailto:quaintspaces@gmail.com" target="_blank">
-              <MdOutlineMail size={24} color={"#3F2A17"} />
+                <TfiEmail size={22} color={"#3F2A17"} />
               <span className="text-[#3F2A17] text-[16px]  ml-2" >
                 :  quaintspaces@gmail.com
               </span>
@@ -53,7 +54,7 @@ export default function Footer() {
             <Link href="https://www.facebook.com/share/64gvciqN2UNPMx6V/?mibextid=LQQJ4d" target="_blank">
               <div className="flex">
                 <FaFacebook color={"#3F2A17"} size={24} />
-                <span className="text-[#3F2A17] text-[16px] capitalize ml-2">: @quaintspacesjaipur</span>
+                <span className="text-[#3F2A17] text-[16px] capitalize ml-2">: Quaint Spaces</span>
               </div>
             </Link>
           </div>
@@ -61,7 +62,7 @@ export default function Footer() {
             <Link href="https://www.instagram.com/quaintspacesjaipur" target="_blank">
               <div className="flex">
                 <FaInstagram color={"#3F2A17"} size={24} />
-                <span className="text-[#3F2A17] text-[16px] uppercase ml-2">: @quaintspacesjaipur</span>
+                <span className="text-[#3F2A17] text-[16px] ml-2">: @quaintspacesjaipur</span>
               </div>
             </Link>
           </div>
