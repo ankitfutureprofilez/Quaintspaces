772b3c38dfc7570ec307faf406d1d7193a857750
bipinfutureprofilez <Bipin@123456789>
2024-07-25 18:18:15 +0530

    working on design feedback

--- pages/login/LoginLogic.js
+++ pages/login/LoginLogic.js
@@ -5,8 +5,7 @@ import { Context } from "../_app";
 import { useContext, useEffect, useState } from "react";
 import Link from "next/link";
 
-export default function LoginLogic({isPopup,color}) {
-
+export default function LoginLogic({ isPopup, color }) {
   const { setAuth, setOpenLogin } = useContext(Context);
   const router = useRouter();
   const [loading, setLoading] = useState(false);
@@ -37,7 +36,7 @@ export default function LoginLogic({isPopup,color}) {
     response
       .then((res) => {
         if (res && res.data && res.data.status) {
-          if(isPopup){
+          if (isPopup) {
             setOpenLogin(false);
           } else {
             router.back();
@@ -63,58 +62,59 @@ export default function LoginLogic({isPopup,color}) {
   };
   return (
     <>
-        <div className="right-signup-form flex justify-end">
-          <div className="signup-form w-full max-h-screen overflow-y-auto">
-            <div className="formbgcolor bg-[#0003]"></div>
-            <div className="quainttay">
-              <h2 className="capitalize">Welcome to Quaintspaces Jaipur</h2>
-              <h3 className={`${color}`}>
-                Don't have an account? <button 
+      <div className="right-signup-form flex justify-end">
+        <div className="signup-form w-full max-h-screen overflow-y-auto">
+          <div className="formbgcolor bg-[#0003]"></div>
+          <div className="quainttay">
+            <h2 className="capitalize">Welcome to Quaintspaces Jaipur</h2>
+            <h3 className={`${color}`}>
+              Don't have an account?{" "}
+              <button
                 className="underline"
                 href="/signup"
-                onClick={()=>{
+                onClick={() => {
                   router.push("/signup");
                   setOpenLogin(false);
                 }}
-                >
-                  Sign up</button>
-              </h3>
-            </div>
-            <form onSubmit={handleSubmit}>
-              <div className="mb-6">
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  className="rounded-md w-full "
-                  required
-                />
-              </div>
-              <div className="mb-6">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  className="rounded-md w-full "
-                  required
-                />
-              </div>
-              <h3 className="text-white text-md  font-medium mb-4">
-                <Link href="/forgot-password">Forgot Password?</Link>
-              </h3>
-              <button type="submit" className="submint-btn">
-                {loading ? "Logging in.." : "Log In"}
+              >
+                Sign up
               </button>
-            </form>
+            </h3>
           </div>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="rounded-md w-full autofill:bg-yellow-200 "
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="rounded-md w-full "
+                required
+              />
+            </div>
+            <h3 className="text-white text-md  font-medium mb-4">
+              <Link href="/forgot-password">Forgot Password?</Link>
+            </h3>
+            <button type="submit" className="submint-btn">
+              {loading ? "Logging in.." : "Log In"}
+            </button>
+          </form>
         </div>
-
+      </div>
     </>
   );
 }
