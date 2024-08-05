4bcc9cdc66de05d9b10c8120f60b83f902236a4c
ankitjain0748 <ankitjain0748@gmail.com>
2024-08-05 17:33:55 +0530

    subscribe

--- pages/book/[listingID].js
+++ pages/book/[listingID].js
@@ -837,7 +837,6 @@ const Book = () => {
                       </h6>
                     </div>
                   </div>
-
                   <div className="flex border-bv border-b mb-4 ">
                     <div className="w-[45%]">
                       <p className="mb-4">
@@ -853,6 +852,48 @@ const Book = () => {
                     </div>
                   </div>
                   {/* {cancelpolicy?.date2 &&
+  <div className="flex ">
+                    <div className="w-[45%] ">
+                      <h6 className="mb-2 text-[18px] font-semibold cancel-policy">
+                    {cancelpolicy?.date2 === new Date()
+                              ? "After"
+                              : "Before"}
+                      </h6>
+                      <h6 className="mb-2 text-2xl font-semibold cancel-policy">
+                        {cancelpolicy?.date
+                          ? ""
+                          : <Dateformat item={formattedCheckIn} /> && "After"}
+                      </h6>
+                    </div>
+                    <div className="w-[55%] pl-3 border-l">
+                      <h6 className="mb-2 text-[18px] font-semibold">
+                      {cancelpolicy?.date === new Date()
+                              ? ""
+                              : "Full Refund"}
+                      </h6>
+                      <h6 className="mb-2">
+                        {cancelpolicy?.date
+                          ? ""
+                          : <Dateformat item={formattedCheckIn} /> &&
+                            "No Refund"}
+                      </h6>
+                    </div>
+                  </div>
+                  <div className="flex border-bv border-b mb-4 ">
+                    <div className="w-[45%]">
+                      <p className="mb-4">
+                        {cancelpolicy?.date2 ? (
+                         <Dateformat item={cancelpolicy?.date2} />
+                        ) : (
+                          <Dateformat item={cancelpolicy} />
+                        )}
+                      </p>
+                    </div>
+                    <div className="w-[55%] pl-3 border-l">
+                      <p className="mb-4">{cancelpolicy?.text}</p>
+                    </div>
+                  </div>
+
                     <div className="mt-5 border-t-2  border-[#efa3a3]  p-4 ">
                       <div className="flex justify-between mb-3 border-b">
                         <div className="w-1/2">
