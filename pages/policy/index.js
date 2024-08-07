8421a570392a78efa2bc8038c7b5873ab50aa651
Naveen Tehrpariya <naveentehrpariya@gmail.com>
2024-08-07 14:34:40 +0530

    merge

--- pages/policy/index.js
+++ pages/policy/index.js
@@ -26,9 +26,7 @@ function index() {
           <div className="max-w-4xl mx-auto p-6">
             <h1 className="text-3xl font-bold my-2">Privacy Policy</h1>
             <div className="prose">
-              
-              
-              <p  >
+              <p>
                 Quaint Spaces exists to help build connections between people and make the world more open and inclusive. In short—to build a world where anyone can belong anywhere. We are a community built on trust. A fundamental part of earning that trust means being clear about how we use your information and protecting your human right to privacy.
               </p>
               <div>
