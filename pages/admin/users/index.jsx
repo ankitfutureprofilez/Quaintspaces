2449382b8874cfb526bb1071138c3063e27d65fe
ankitjain0748 <ankitjain0748@gmail.com>
2024-08-26 17:35:27 +0530

    user reactvie

--- pages/admin/users/index.jsx
+++ pages/admin/users/index.jsx
@@ -193,48 +193,47 @@ export default function Index() {
                 <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap ">
                   <div className="flex items-center gap-1 p-1">
                     <button
-                      onClick={() =>statusUpdate(item.id, item.status === 0 ? 1 : 0)}
-                      >
-                        
-                    {item.status === 0 ? (
-                      <div className="flex items-center border rounded-full px-2 py-1">
-                        <p className="text-xs me-1">Disable</p>
-                        <svg
-                          className="text-gray-400"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                        >
-                          <path
-                            d="m19.53 5.53-14 14c-.02.02-.03.03-.05.04-.38-.32-.73-.67-1.05-1.05A9.903 9.903 0 0 1 2 12C2 6.48 6.48 2 12 2c2.49 0 4.77.91 6.52 2.43.38.32.73.67 1.05 1.05-.01.02-.02.03-.04.05ZM22 12c0 5.49-4.51 10-10 10-1.5 0-2.92-.33-4.2-.93-.62-.29-.74-1.12-.26-1.61L19.46 7.54c.48-.48 1.32-.36 1.61.26.6 1.27.93 2.7.93 4.2Z"
-                            fill="currentColor"
-                          ></path>
-                          <path
-                            d="M21.77 2.229c-.3-.3-.79-.3-1.09 0L2.23 20.689c-.3.3-.3.79 0 1.09a.758.758 0 0 0 1.08-.01l18.46-18.46c.31-.3.31-.78 0-1.08Z"
-                            fill="currentColor"
-                          ></path>
-                        </svg>
-                      </div>
-                    ) : (
-                      <div className="flex items-center border rounded-full px-2 py-1">
-                        <p className="text-xs me-1">Enable</p>
-                        <svg
-                          className="text-emerald-500"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                        >
-                          <path
-                            d="M12 2C6.49 2 2 6.49 2 12s4.49 10 10 10 10-4.49 10-10S17.51 2 12 2Zm4.78 7.7-5.67 5.67a.75.75 0 0 1-1.06 0l-2.83-2.83a.754.754 0 0 1 0-1.06c.29-.29.77-.29 1.06 0l2.3 2.3 5.14-5.14c.29-.29.77-.29 1.06 0 .29.29.29.76 0 1.06Z"
-                            fill="currentColor"
-                          ></path>
-                        </svg>
-                      </div>
-                    )}
+                      onClick={() => statusUpdate(item.id, item.status === 0 ? 1 : 0)}
+                    >
+                      {item.status === 0 ? (
+                        <div className="flex items-center border rounded-full px-2 py-1">
+                          <p className="text-xs me-1">Disable</p>
+                          <svg
+                            className="text-gray-400"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                          >
+                            <path
+                              d="m19.53 5.53-14 14c-.02.02-.03.03-.05.04-.38-.32-.73-.67-1.05-1.05A9.903 9.903 0 0 1 2 12C2 6.48 6.48 2 12 2c2.49 0 4.77.91 6.52 2.43.38.32.73.67 1.05 1.05-.01.02-.02.03-.04.05ZM22 12c0 5.49-4.51 10-10 10-1.5 0-2.92-.33-4.2-.93-.62-.29-.74-1.12-.26-1.61L19.46 7.54c.48-.48 1.32-.36 1.61.26.6 1.27.93 2.7.93 4.2Z"
+                              fill="currentColor"
+                            ></path>
+                            <path
+                              d="M21.77 2.229c-.3-.3-.79-.3-1.09 0L2.23 20.689c-.3.3-.3.79 0 1.09a.758.758 0 0 0 1.08-.01l18.46-18.46c.31-.3.31-.78 0-1.08Z"
+                              fill="currentColor"
+                            ></path>
+                          </svg>
+                        </div>
+                      ) : (
+                        <div className="flex items-center border rounded-full px-2 py-1">
+                          <p className="text-xs me-1">Enable</p>
+                          <svg
+                            className="text-emerald-500"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                          >
+                            <path
+                              d="M12 2C6.49 2 2 6.49 2 12s4.49 10 10 10 10-4.49 10-10S17.51 2 12 2Zm4.78 7.7-5.67 5.67a.75.75 0 0 1-1.06 0l-2.83-2.83a.754.754 0 0 1 0-1.06c.29-.29.77-.29 1.06 0l2.3 2.3 5.14-5.14c.29-.29.77-.29 1.06 0 .29.29.29.76 0 1.06Z"
+                              fill="currentColor"
+                            ></path>
+                          </svg>
+                        </div>
+                      )}
                     </button>
                   </div>
                 </td>
@@ -307,16 +306,16 @@ export default function Index() {
                         >
                           <li className="block px-4 py-2 hover:bg-gray-100">
                             <button
-                              // onClick={() => {
-                              //   handleRowClick(null);
-                              //   statusUpdate(item.id,item.status === 0 ? 1 : 0)
-                              // }
-                              // }
+                            // onClick={() => {
+                            //   handleRowClick(null);
+                            //   statusUpdate(item.id,item.status === 0 ? 1 : 0)
+                            // }
+                            // }
                             >
                               {item.status === 0 ? (
-                                  <p className="text-normal">Enable User</p>
-                              ) : 
-                                  <p className="text-normal">Suspend User</p> 
+                                <p className="text-normal">Enable User</p>
+                              ) :
+                                <p className="text-normal">Suspend User</p>
                               }
                             </button>
                           </li>
